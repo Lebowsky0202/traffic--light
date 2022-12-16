5a4ce3c866b93b459946fdce477215a96c4cfa2b
Kuat <tipoproger@gmail.com>
2022-12-17 01:40:29 +0600

    final

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+
 'use strict'
 
+const lights = document.querySelectorAll('.light');
+
+let activeLight = 0;
+
+setInterval(() => {
+    changeLight();
+}, 2000);
+
+function changeLight() {
+    lights[activeLight].className = 'light';
+    activeLight++;
+
+    if(activeLight > 2){
+        activeLight = 0;
+    }
+
+    const currentLight = lights[activeLight];
+    currentLight.classList.add(currentLight.getAttribute('color'))
+}
